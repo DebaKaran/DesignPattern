c4b00a6777c862857f2496afdf0ad46416f6db17
Deba Karan Agrawala <karan.deba@gmail.com>
2025-12-18 21:55:53 +0530

    Added null type checks to prevent potential NullPointerExceptions in ReportClient.java

---
+++ src/com/creational/factory/client/ReportClient.java
@@ -0,0 +1,29 @@
+package com.creational.factory.client;
+
+import com.creational.factory.model.Report;
+import com.creational.factory.services.CsvReport;
+import com.creational.factory.services.ExcelReport;
+import com.creational.factory.services.PdfReport;
+import com.creational.factory.type.ReportType;
+
+public class ReportClient {
+
+    public Report getReport(final ReportType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Report type cannot be null");
+        }
+
+        switch (type) {
+            case CSV -> {
+                return new CsvReport();
+            }
+            case PDF -> {
+                return new PdfReport();
+            }
+            case EXCEL -> {
+                return new ExcelReport();
+            }
+            default -> throw new IllegalArgumentException("Unsupported report type");
+        }
+    }
+}
